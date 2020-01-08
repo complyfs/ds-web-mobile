bcc90c0fe7ec064bbd0c5cad07fe2d1bbaad0ce9
Patrick N <pnetsmail-ms@yahoo.com>
2020-01-08 14:45:53 +0100

    Remove default paths and send all to 'private'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,8 +24,8 @@ import { PremiumSubscriberComponent } from './private/premium-subscriber/premium
 import {BillingComponent} from './private/billing/billing.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/public/home', pathMatch: 'full' },
-  { path: 'public', component: PublicRootComponent,
+  { path: '', redirectTo: '/private', pathMatch: 'full' },
+/*  { path: 'public', component: PublicRootComponent,
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
@@ -33,30 +33,30 @@ const routes: Routes = [
       { path: 'callback', component: CallbackComponent },
       { path: 'loginerror/:errorType', component: LoginErrorComponent }
     ]
-  },
+  },*/
   {
     path: 'private', component: PrivateRootComponent, canActivate: [AuthenticatedGuard], canActivateChild: [AuthenticatedGuard],
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-      { path: 'basicsub', component: BasicSubscriberComponent },
+      { path: 'dashboard', component: DashboardComponent },
+/*      { path: 'basicsub', component: BasicSubscriberComponent },
       { path: 'premsub', component: PremiumSubscriberComponent },
       { path: 'profile', component: ProfileComponent },
       { path: 'account', component: AccountComponent },
       { path: 'billing', component: BillingComponent },
-      { path: 'dashboard', component: DashboardComponent },
       { path: 'signup', component: SignupComponent },
       { path: 'signupthanks', component: StripeThanksComponent },
-      { path: 'signupcancel', component: StripeCancelComponent }
+      { path: 'signupcancel', component: StripeCancelComponent }*/
     ]
   },
-  {
+/*  {
     path: 'admin', component: AdminRootComponent,  canActivate: [AuthorizedGuard], canActivateChild: [AuthorizedGuard], data: {expectedRoles: ['Admin']},
     children: [
       { path: '', redirectTo: 'users', pathMatch: 'full'},
       { path: 'users', component: Auth0UsersComponent, data: {expectedRoles: ['Admin']} },
       { path: 'roles', component: Auth0RolesComponent, data: {expectedRoles: ['Admin']} }
     ]
-  }
+  }*/
 ];
 
 @NgModule({
