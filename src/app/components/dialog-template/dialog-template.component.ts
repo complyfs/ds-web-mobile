3d83edd26cd6cfcc508cf0a26f552955e9bef2f2
Patrick N <pnetsmail-ms@yahoo.com>
2020-01-12 18:46:41 +0100

    Init commit

---
+++ src/app/components/dialog-template/dialog-template.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, ContentChild, EventEmitter,  Output,  TemplateRef, ChangeDetectionStrategy } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-template',
+  templateUrl: './dialog-template.component.html',
+  styleUrls: ['./dialog-template.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DialogTemplateComponent implements OnInit {
+
+  @ContentChild(TemplateRef, /* TODO: add static flag */ { static: false }) template: TemplateRef<{}>;
+  @Output() result = new EventEmitter();
+
+  dRef: MatDialogRef<any>;
+
+  constructor (private dialog: MatDialog) {}
+
+  ngOnInit() {}
+
+  open () {
+    this.dRef = this.dialog.open(this.template, {panelClass: 'cfs-modal-dialog'});
+    this.dRef.afterClosed().subscribe(val => this.result.emit(val));
+  }
+
+}
