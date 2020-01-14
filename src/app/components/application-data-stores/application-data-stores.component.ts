af811e95e1c7342327f2c7710eaed42182c08bc1
Patrick N <pnetsmail-ms@yahoo.com>
2020-01-14 18:07:16 +0100

    Add ApplicationUsersComponent, ApplicationDetailComponent, ApplicationDataStoresComponent

---
+++ src/app/components/application-data-stores/application-data-stores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-application-data-stores',
+  templateUrl: './application-data-stores.component.html',
+  styleUrls: ['./application-data-stores.component.scss']
+})
+export class ApplicationDataStoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/application-detail/application-detail.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {RestService} from "../../services/rest/rest.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {finalize} from "rxjs/operators";
+import {environment} from "../../../environments/environment";
+
+@Component({
+  selector: 'app-application-detail',
+  templateUrl: './application-detail.component.html',
+  styleUrls: ['./application-detail.component.scss']
+})
+export class ApplicationDetailComponent implements OnInit {
+
+  @Input() applicationId: string;
+  application: any;
+
+  loading: boolean = false;
+
+  constructor(private restService: RestService,
+              private snackMessage: MatSnackBar) { }
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    this.loading = true;
+
+    const params = { _id: this.applicationId };
+
+    this.restService.adminGetApplication(params)
+      .pipe(
+        finalize(() => { this.loading = false; })
+      )
+      .subscribe ( r => {
+        this.application = r;
+      }, e => {
+        this.snackMessage.open('Error getting application data', 'x', {verticalPosition: 'top'});
+      });
+  }
+
+  saveApplication() {
+    this.restService.adminSaveApplication(this.application)
+      .subscribe( r => {
+        this.snackMessage.open('Application saved', null,{ duration: environment.snackBarDuration, verticalPosition: 'bottom' });
+        this.loadData();
+      }, err => {
+        this.snackMessage.open('Error saving Application', 'x', {verticalPosition: 'top'});
+      });
+  }
+}
