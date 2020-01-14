8362a7481bc2ba7b53ed533387d00c7b2cc2654a
Patrick N <pnetsmail-ms@yahoo.com>
2020-01-14 11:29:17 +0100

    add TextFieldComponent, TextAreaComponent, ApplicationPanelComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,9 @@ import { LogsComponent } from './admin/logs/logs.component';
 import { TenantsComponent } from './admin/tenants/tenants.component';
 import { DialogTemplateComponent } from './components/dialog-template/dialog-template.component';
 import { ApplicationsComponent } from './admin/applications/applications.component';
+import { TextFieldComponent } from './components/field-display/text-field/text-field.component';
+import { TextAreaComponent } from './components/field-display/text-area/text-area.component';
+import { ApplicationPanelComponent } from './components/application-panel/application-panel.component';
 
 @NgModule({
   declarations: [
@@ -66,6 +69,9 @@ import { ApplicationsComponent } from './admin/applications/applications.compone
     TenantsComponent,
     DialogTemplateComponent,
     ApplicationsComponent,
+    TextFieldComponent,
+    TextAreaComponent,
+    ApplicationPanelComponent,
   ],
   imports: [
     BrowserModule,
